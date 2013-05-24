868aba36feabb33705c03a7be90332638f80f9aa
Pamela Nibaldi <pamela.nibaldi@globant.com>
2013-05-24 16:19:01 -0300

    .

--- Topic 6/js/app.js
+++ Topic 6/js/app.js
@@ -21,32 +21,32 @@
 // Start the main app logic.
 // requirejs(['jquery', 'underscore', 'backbone'],
 var model = function () {
-
-  var Movie = (function() {
-    var Movie = function(){this.attributes = {};};
-    Movie.prototype.constructor = Movie;
-    Movie.prototype.set = function (attr, value) {
+  var Movie = Backbone.Model.extend({
+    constructor: function() {
+      this.attributes = {};
+      Backbone.Model.apply(this, arguments);
+    },
+    set: function(attr, value) {
       this.attributes[attr] = value;
-    };
-    Movie.prototype.get = function (attr) {
+    },
+    get: function(attr) {
       return this.attributes[attr];
-    };
-    return Movie;
-  })();
+    }
+  });
   // model constructor function
-  //
   var makeEmailModel = function(data) {
       var movie = new Movie();
       movie.set('title', data.title);
       movie.set('director', data.director);
       movie.set('year', data.year);
+      console.log(movie);
       return movie;
   };
 
   // load data from the server and create
   // model objects
   //
-  var loadMovies = function(callbacks) {
+  var loadMovies = function() {
     var json = null;
     $.ajax({
       'async': false,
@@ -61,4 +61,6 @@ var model = function () {
       }
     });
   };
+  loadMovies();
 };
+model();
